a7f01f12f3f467e1f28201f5343770f0a59e94e3
Emily Asch <e.asch@me.com>
2018-10-24 18:47:09 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,10 +1,171 @@
 # Write your code here!
+require "pry"
 
+def game_hash
+  {
+    :home => {
+      :name => "Brooklyn Nets",
+      :colors => ["Black", "White"],
+      :players => [
+        {
+          :name => "Alan Anderson",
+          :number => 0,
+          :shoe => 16,
+          :points => 22,
+          :rebouds => 12,
+          :assists => 12,
+          :steals => 3,
+          :blocks => 1,
+          :slam_dunks => 1,
+        },
+        {
+          :name => "Reggie Evans",
+          :number => 30,
+          :shoe => 14,
+          :points => 12,
+          :rebouds => 12,
+          :assists => 12,
+          :steals => 12,
+          :blocks => 12,
+          :slam_dunks => 7,
+        },
+         {
+          :name => "Brook Lopez",
+          :number => 11,
+          :shoe => 17,
+          :points => 17,
+          :rebouds => 19,
+          :assists => 10,
+          :steals => 3,
+          :blocks => 1,
+          :slam_dunks => 15,
+        },
+         {
+          :name => "Mason Plumlee",
+          :number => 1,
+          :shoe => 19,
+          :points => 26,
+          :rebouds => 12,
+          :assists => 6,
+          :steals => 3,
+          :blocks => 8,
+          :slam_dunks => 5,
+        },
+         {
+          :name => "Jason Terry",
+          :number => 31,
+          :shoe => 15,
+          :points => 19,
+          :rebouds => 2,
+          :assists => 2,
+          :steals => 4,
+          :blocks => 11,
+          :slam_dunks => 1,
+        }
+        ]
+    },
+:away => {
+      :name => "Charlotte Hornets",
+      :colors => ["Turqoise", "Purple"],
+      :players => [
+        {
+          :name => "Jeff Adrien",
+          :number => 4,
+          :shoe => 18,
+          :points => 10,
+          :rebouds => 1,
+          :assists => 1,
+          :steals => 2,
+          :blocks => 7,
+          :slam_dunks => 2,
+        },
+        {
+          :name => "Bismak Biyombo",
+          :number => 0,
+          :shoe => 16,
+          :points => 12,
+          :rebouds => 4,
+          :assists => 7,
+          :steals => 7,
+          :blocks => 15,
+          :slam_dunks => 10,
+        },
+        {
+          :name => "DeSagna Diop",
+          :number => 2,
+          :shoe => 14,
+          :points => 24,
+          :rebouds => 12,
+          :assists => 12,
+          :steals => 4,
+          :blocks => 5,
+          :slam_dunks => 5,
+        },
+        {
+          :name => "Ben Gordon",
+          :number => 8,
+          :shoe => 15,
+          :points => 33,
+          :rebouds => 3,
+          :assists => 2,
+          :steals => 1,
+          :blocks => 1,
+          :slam_dunks => 0,
+        },
+        {
+          :name => "Brendan Haywood",
+          :number => 33,
+          :shoe => 15,
+          :points => 6,
+          :rebouds => 12,
+          :assists => 12,
+          :steals => 22,
+          :blocks => 5,
+          :slam_dunks => 12,
+        }
+        ]
+      }
+  }
+end
 
+def good_practices
+  game_hash.each do |location, team_data|
+    binding.pry
+team_data.each do |attribute, data|
+  binding.pry
+  data.each do |data_item|
+            binding.pry
+      end
+    end
+  end
+end
 
+def num_points_scored(name,num_points_scored)
+  answer = nil
+  game.each do |team, points|
+    players_array = points[:players]
+    players_array.each do |player_points|
+      if player_points[:name] == name
+        answer = player_points[:points]
+      end
+    end
+  end
+answer
+end
 
+def shoe_size(name,shoe_size)
+  answer = nil
+   game.each do |team, shoe|
+    players_array = shoe[:players]
+    players_array.each do |player_shoe|
+      if player_shoe[:name] == name
+        answer = player_shoe[:shoe]
+      end
+    end
+  end
+answer
 
-
+end
 
 
 
