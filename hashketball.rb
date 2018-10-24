43f8bfdccecf4ada1358238b74069a9e383b4002
Emily Asch <e.asch@me.com>
2018-10-24 19:15:58 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -182,7 +182,7 @@ def team_name
 
 end
 
-def player_numbers(team_name)
+def player_numbers(team)
 game_hash.values.each do |team_num|
   if team_num.has_value?(team_name)
     return team_num[:players].map {|player|player[:number]}
