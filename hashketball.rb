76f6be72f3b20c21316364f60cef17e170213ce1
Emily Asch <e.asch@me.com>
2018-10-24 19:20:03 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,7 +5,7 @@ def game_hash
   game_hash = {
     :home => {
       :team_name => "Brooklyn Nets",
-      :colors => ["Black","White"],
+      :colors => ['black','white'],
       :players => [
         {
           :name => "Alan Anderson",
