e52103d2614e22e014470a01f5b6031390ad618a
Emily Asch <e.asch@me.com>
2018-10-24 19:20:18 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -12,7 +12,7 @@ def game_hash
           :number => 0,
           :shoe => 16,
           :points => 22,
-          :rebouds => 12,
+          :rebounds => 12,
           :assists => 12,
           :steals => 3,
           :blocks => 1,
